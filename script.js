06c136d8d4cfa8d9b9402cc829785891b9c6268c
VishalKumar3107 <116445326+VishalKumar3107@users.noreply.github.com>
2023-01-23 21:01:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
- class API {
-        #secure;
+ #secure;
         constructor(url, method = 'GET') {
           this.url = url;
           this.method = method;
           this.#secure = url.startsWith('https') ? true : false;
         }
+
         get secure(){
           return this.#secure;
         }
         isSecure() {
           return this.#secure;
         }
-  
+
         updateURL(url) {
           this.url = url;
           this.#secure = url.startsWith('https') ? true : false;
         }
       }
-      const s = new API('http://api.com/api/hello');
       
-      document.querySelector('#check-security').addEventListener('click', function(){
-        console.log(s.isSecure()); // false
-        s.updateURL('https://api.com/api/hello');
-        console.log(s.isSecure()); // true
-        console.log(s.url); // https://api.com/api/hello
-        console.log(s.method); // GET
-        console.log(s.secure); // undefined (because private field)
+      const urlInput = document.querySelector("#url");
+      const methodSelect = document.querySelector("#method");
+      const checkSecurityBtn = document.querySelector("#check-security");
+      const outputDiv = document.querySelector("#output");
+      
+      let api = new API(urlInput.value, methodSelect.value);
+      
+      checkSecurityBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        api.updateURL(urlInput.value);
+        let isSecure = api.isSecure() ? "Secure" : "Not Secure";
+        let output = `URL: ${api.url}<br>Method: ${api.method}<br>Security: ${isSecure}`;
+        outputDiv.innerHTML = output;
       });
